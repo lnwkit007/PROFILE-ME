329475aea09255d7cd0fac8f0f712465c10a9e83
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-07-27 14:06:50 +0700

    Update animation in component Hero.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,35 +1,94 @@
+<script setup lang="ts">
+import { easeOut, motion } from "motion-v";
+</script>
+
 <template>
   <section
     class="hero relative w-full overflow-hidden flex justify-center sm:items-center"
   >
     <!-- ////////// Background Shadow ////////// -->
-    <img
-      src="/image/hero/background-shadow-1.png"
-      alt="background-shadow-1"
-      class="absolute left-1/2 -bottom-25 -translate-x-1/2 z-0 w-full min-w-360 h-auto object-cover pointer-events-none"
-    />
+    <motion.div
+      :initial="{
+        opacity: 0,
+      }"
+      :animate="{
+        opacity: 1,
+      }"
+      :transition="{
+        duration: 1,
+        ease: easeOut,
+      }"
+    >
+      <img
+        src="/image/hero/background-shadow-1.png"
+        alt="background-shadow-1"
+        class="absolute left-1/2 -bottom-25 -translate-x-1/2 z-0 w-full min-w-360 h-auto object-cover pointer-events-none"
+      />
+    </motion.div>
 
     <!-- ////////// My Name ////////// -->
-    <div class="mt-16 sm:mt-0">
-      <h1
-        class="ml-2 sm:ml-0 z-3 jersey-10-regular text-center text-[clamp(105px,35vw,185px)] sm:text-[225px] md:text-[275px] lg:text-[365px] xl:text-[425px] bg-[linear-gradient(180deg,#E4E4E4_0%,#0C0503_67%)] bg-clip-text text-transparent"
-      >
-        KITSADA
-      </h1>
-    </div>
+    <motion.div
+      :initial="{
+        opacity: 0,
+      }"
+      :animate="{
+        opacity: 1,
+      }"
+      :transition="{
+        duration: 1,
+        delay: 1.5,
+        ease: easeOut,
+      }"
+    >
+      <div class="mt-16 sm:mt-0">
+        <h1
+          class="ml-2 sm:ml-0 z-3 jersey-10-regular text-center text-[clamp(105px,35vw,185px)] sm:text-[225px] md:text-[275px] lg:text-[365px] xl:text-[425px] bg-[linear-gradient(180deg,#E4E4E4_0%,#0C0503_67%)] bg-clip-text text-transparent"
+        >
+          KITSADA
+        </h1>
+      </div>
+    </motion.div>
 
     <!-- ////////// Image Me ////////// -->
-    <img
-      src="/image/hero/kitsada.png"
-      alt="kitsada"
-      class="z-4 absolute bottom-0 left-1/2 -translate-x-1/2 w-[clamp(570px,35vw,630px)] min-h-118 sm:h-auto object-cover"
-    />
+    <motion.div
+      :initial="{
+        opacity: 0,
+      }"
+      :animate="{
+        opacity: 1,
+      }"
+      :whileInView="{ opacity: 1 }"
+      :transition="{
+        duration: 1,
+        delay: 0.75,
+        ease: easeOut,
+      }"
+    >
+      <img
+        src="/image/hero/kitsada.png"
+        alt="kitsada"
+        class="z-4 absolute bottom-0 left-1/2 -translate-x-1/2 w-[clamp(570px,35vw,630px)] min-h-118 sm:h-auto object-cover"
+      />
+    </motion.div>
 
     <!-- ////////// Background Shadow ////////// -->
-    <img
-      src="/image/hero/background-shadow-2.png"
-      alt="background-shadow-2"
-      class="z-5 absolute -bottom-20 left-1/2 -translate-x-1/2 w-full min-w-360 h-auto object-cover pointer-events-none"
-    />
+    <motion.div
+      :initial="{
+        opacity: 0,
+      }"
+      :animate="{
+        opacity: 1,
+      }"
+      :transition="{
+        duration: 1,
+        ease: easeOut,
+      }"
+    >
+      <img
+        src="/image/hero/background-shadow-2.png"
+        alt="background-shadow-2"
+        class="z-5 absolute -bottom-20 left-1/2 -translate-x-1/2 w-full min-w-360 h-auto object-cover pointer-events-none"
+      />
+    </motion.div>
   </section>
 </template>
--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import { easeOut, motion } from "motion-v";
 
 // import stores
 import { useOpenSidebarStore } from "../stores/openSidebarStore";
@@ -7,8 +8,7 @@ const SidebarStore = useOpenSidebarStore();
 
 const openSidebar = () => {
   SidebarStore.SwitchSidebar();
-}
-
+};
 
 const menulists = [
   {
@@ -31,55 +31,69 @@ const menulists = [
 </script>
 
 <template>
-  <nav class="fixed w-full top-0 z-200 p-2">
-    <div
-      class="flex items-center justify-between mx-auto max-w-330 px-6 py-2 bg-white/5 border border-white/10 rounded-full backdrop-blur-lg"
-    >
-      <RouterLink
-        to="/#hero"
-        class="jersey-10-regular flex gap-2 text-3xl sm:text-4xl cursor-pointer"
+  <motion.div
+    :initial="{
+      opacity: 0,
+    }"
+    :animate="{
+      opacity: 1,
+    }"
+    :transition="{
+      duration: 1,
+      delay: 2,
+      ease: easeOut,
+    }"
+  >
+    <nav class="fixed w-full top-0 z-200 p-2">
+      <div
+        class="flex items-center justify-between mx-auto max-w-330 px-6 py-2 bg-white/5 border border-white/10 rounded-full backdrop-blur-lg"
       >
-        <p
-          class="bg-[linear-gradient(180deg,#E4E4E4_0%,#0C0503_90%)] bg-clip-text text-transparent"
+        <RouterLink
+          to="/#hero"
+          class="jersey-10-regular flex gap-2 text-3xl sm:text-4xl cursor-pointer"
         >
-          PROFILE
-        </p>
-        <p
-          class="bg-[linear-gradient(180deg,#F55303_0%,#632701_90%)] bg-clip-text text-transparent"
-        >
-          ME
-        </p>
-      </RouterLink>
-
-      <button class="block sm:hidden" @click="openSidebar">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          color="#7C7978"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-text-align-justify-icon lucide-text-align-justify"
-        >
-          <path d="M3 5h18" />
-          <path d="M3 12h18" />
-          <path d="M3 19h18" />
-        </svg>
-      </button>
-
-      <ul class="hidden sm:flex gap-5 jersey-10-regular text-3xl">
-        <RouterLink v-for="item in menulists" :to="item.path">
-          <li
-            class="cursor-pointer bg-[linear-gradient(180deg,#E4E4E4_0%,#0C0503_90%)] bg-clip-text text-transparent"
+          <p
+            class="bg-[linear-gradient(180deg,#E4E4E4_0%,#0C0503_90%)] bg-clip-text text-transparent"
           >
-            {{ item.name }}
-          </li>
+            PROFILE
+          </p>
+          <p
+            class="bg-[linear-gradient(180deg,#F55303_0%,#632701_90%)] bg-clip-text text-transparent"
+          >
+            ME
+          </p>
         </RouterLink>
-      </ul>
-    </div>
-  </nav>
+
+        <button class="block sm:hidden" @click="openSidebar">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            color="#7C7978"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-text-align-justify-icon lucide-text-align-justify"
+          >
+            <path d="M3 5h18" />
+            <path d="M3 12h18" />
+            <path d="M3 19h18" />
+          </svg>
+        </button>
+
+        <ul class="hidden sm:flex gap-5 jersey-10-regular text-3xl">
+          <RouterLink v-for="item in menulists" :to="item.path">
+            <li
+              class="cursor-pointer bg-[linear-gradient(180deg,#E4E4E4_0%,#0C0503_90%)] bg-clip-text text-transparent"
+            >
+              {{ item.name }}
+            </li>
+          </RouterLink>
+        </ul>
+      </div>
+    </nav>
+  </motion.div>
 </template>
